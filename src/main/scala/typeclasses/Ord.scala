b53cdf6f1d8b31dbd677f5bc2db61d81156eb724
Jeremy Smith <jeremys@netflix.com>
2018-12-15 09:09:26 +0000

    Solution for Ord derivations

--- src/main/scala/typeclasses/Ord.scala
+++ src/main/scala/typeclasses/Ord.scala
@@ -23,12 +23,65 @@ object Ord {
   // HList induction - for products (case classes) //
   ///////////////////////////////////////////////////
 
-  // TODO - we'll fill this in
+  // This is the base case for induction - HNil is always equal to itself
+  implicit val hnilOrd: Ord[HNil] = new Ord[HNil] {
+    def compare(a: HNil, b: HNil): Int = 0
+  }
+
+  // This is the recursive case - compare the current element, and if they're equal then
+  // continue comparing the rest of the elements
+  implicit def hlistOrd[H, T <: HList](implicit
+    ordH: Ord[H],
+    ordT: Ord[T]
+  ): Ord[H :: T] = new Ord[H :: T] {
+    def compare(a: H :: T, b: H :: T): Int = ordH.compare(a.head, b.head) match {
+      case 0   => ordT.compare(a.tail, b.tail)
+      case neq => neq
+    }
+  }
+
+  // this will handle case classes by requiring the induction over the HList
+  implicit def genericProductOrd[T <: Product, L <: HList](implicit
+    gen: Generic.Aux[T, L],
+    ordL: Lazy[Ord[L]]
+  ): Ord[T] = new Ord[T] {
+    def compare(a: T, b: T): Int = ordL.value.compare(gen.to(a), gen.to(b))
+  }
 
   ////////////////////////////////////////////////////////
   // Coproduct induction - for sealed trait heirarchies //
   ////////////////////////////////////////////////////////
 
-  // TODO - we'll fill this in
+  // the base case for coproduct induction – this instance won't ever be called
+  implicit val cnilOrd: Ord[CNil] = new Ord[CNil] {
+    def compare(a: CNil, b: CNil): Int = 0
+  }
+
+  // this handles case objects (which otherwise wouldn't have an instance)
+  implicit def singleton[A <: Singleton](implicit witness: Witness.Aux[A]): Ord[A] = new Ord[A] {
+    def compare(a: A, b: A): Int = 0
+  }
+
+  // this handles inductive coproducts - if they're the same type, compare them; otherwise
+  // the earlier type in the coproduct is smaller (to match Odersky's logic)
+  implicit def coproductOrd[H, T <: Coproduct](implicit
+    ordH: Ord[H],
+    ordT: Ord[T]
+  ): Ord[H :+: T] = new Ord[H :+: T] {
+    def compare(a: H :+: T, b: H :+: T): Int = (a, b) match {
+      case (Inl(ha), Inl(hb)) => ordH.compare(ha, hb)
+      case (Inr(ta), Inr(tb)) => ordT.compare(ta, tb)
+      case (Inl(_), _) => -1
+      case (Inr(_), _) => 1
+    }
+  }
+
+  // this will handle sealed trait hierarchies by requiring the coproduct induction
+  implicit def genericCoproductOrd[T, C <: Coproduct](implicit
+    gen: Generic.Aux[T, C],
+    ordC: Lazy[Ord[C]]
+  ): Ord[T] = new Ord[T] {
+    def compare(a: T, b: T): Int = ordC.value.compare(gen.to(a), gen.to(b))
+  }
 
 }
